d7e982649ad77bcd498d78c143886af05849cd5b
Scott Chandler <gsc@woolybeastsoftware.com>
2015-06-21 13:45:29 -0700

    Added licensing header

--- WBLayoutView/WBLayoutView.h
+++ WBLayoutView/WBLayoutView.h
@@ -3,8 +3,28 @@
 //  WBLayoutView
 //
 //  Created by Scott Chandler on 6/15/15.
-//  Copyright (c) 2015 Wooly Beast Software LLC. All rights reserved.
 //
+//	The MIT License (MIT)
+//
+//	Copyright (c) 2015 Wooly Beast Software LLC
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//	SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
@@ -17,16 +37,16 @@ typedef NS_ENUM(NSInteger, WBLayoutViewAlignment) {
 	WBLayoutViewAlignmentCenter,
 };
 
-// future work
 typedef NS_ENUM(NSInteger, WBLayoutViewDistribution) {
 	WBLayoutViewDistributionEqualSpacing
+	// more forthcoming...
 };
 
 @interface WBLayoutView : UIView
-@property (nonatomic) UILayoutConstraintAxis axis;
-@property (nonatomic) WBLayoutViewAlignment alignment;
-@property (nonatomic) WBLayoutViewDistribution distribution;
-@property (nonatomic) CGFloat spacing;
+@property (nonatomic) WBLayoutViewAlignment		alignment;
+@property (nonatomic) UILayoutConstraintAxis		axis;
+@property (nonatomic) WBLayoutViewDistribution	distribution;
+@property (nonatomic) CGFloat					spacing;
 
 @property (nonatomic,copy) NSArray *arrangedSubviews;
 - (void)addArrangedSubview:(UIView *)subview;
